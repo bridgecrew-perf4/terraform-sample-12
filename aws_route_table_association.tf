e05a9c5ebf875c259422a0eb5e17f8c18f45bcbd
‘ty’ <bv9t-yn@live.jp>
2021-02-09 15:36:35 +0900

    create module of security_group

--- aws_route_table_association.tf
+++ aws_route_table_association.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public_ecs.id
+  subnet_id      = aws_subnet.public_ecs.id
   route_table_id = aws_route_table.public.id
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "test_name" {
   value = "test value"
 }
+
+output "public_dns" {
+  value = aws_instance.default.public_dns
+}
